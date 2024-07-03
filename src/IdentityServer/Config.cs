14295bde4509f2b54861db5d2154617cbeccb951
Tom Hooper <tom.hooper@spektrix.com>
2024-07-03 17:24:53 +0100

    Configure OIDC Scopes

--- src/IdentityServer/Config.cs
+++ src/IdentityServer/Config.cs
@@ -6,8 +6,9 @@ public static class Config
 {
     public static IEnumerable<IdentityResource> IdentityResources =>
         new IdentityResource[]
-        { 
-            new IdentityResources.OpenId()
+        {
+            new IdentityResources.OpenId(),
+            new IdentityResources.Profile(),
         };
 
     public static IEnumerable<ApiScope> ApiScopes =>
