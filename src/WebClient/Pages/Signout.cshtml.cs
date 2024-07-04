5b9fc8525b4067a5f816045931c8bbec5d5fe37d
Tom Hooper <tom.hooper@spektrix.com>
2024-07-04 17:01:50 +0100

    Add Signout flow

---
+++ src/WebClient/Pages/Signout.cshtml.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyApp.Namespace
+{
+    public class SignoutModel : PageModel
+    {
+        public IActionResult OnGet()
+        {
+            return SignOut("Cookies", "oidc");
+        }
+    }
+
+}
