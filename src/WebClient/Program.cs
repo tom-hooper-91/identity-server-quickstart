1562b03360f184f737241b21222fb7fb3126a838
Tom Hooper <tom.hooper@spektrix.com>
2024-07-04 16:43:48 +0100

    Configure Pipeline and display auth cookie

--- src/WebClient/Program.cs
+++ src/WebClient/Program.cs
@@ -40,9 +40,9 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
-app.MapRazorPages();
+app.MapRazorPages().RequireAuthorization();
 
 app.Run();
